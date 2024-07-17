ae52bc529b154ab846b2e2a208474760a017fc64
wangxiaoran <amber20030707@gmail.com>
2024-07-17 09:38:37 +0800

    stack his

---
+++ conta_nolog.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.colors as mcolors
+
+# 加载数据
+data= pd.read_excel(r'/Users/wangxiaoran/Desktop/20240528 画图需求.xlsx', sheet_name='conta_proportion')
+
+# 重命名列以便于处理
+data_avg_concentration = data[['adulterant_english', 'monitoring_province', 'ProbofAntiForM']]
+data_avg_concentration.columns = ['adulterant', 'monitoring_province', 'ProbofAntiForM']
+
+# 取以2为底的log
+data_avg_concentration['LOG_ProbofAntiForM'] =data_avg_concentration['ProbofAntiForM']
+
+# 创建数据透视表
+pivot_data_avg_concentration_log = data_avg_concentration.pivot_table(index='monitoring_province', columns='adulterant', values='LOG_ProbofAntiForM', fill_value=0)
+
+# 绘图
+plt.figure(figsize=(14, 10))
+sns.set(font_scale=1.2)  # 增大字体
+
+cmap = "coolwarm"
+# 增加颜色断点数量，并限制到0-1范围
+boundaries = np.linspace(0, 1, 100)
+norm = mcolors.BoundaryNorm(boundaries, ncolors=256)
+#作图
+heatmap = sns.heatmap(pivot_data_avg_concentration_log, annot=True, cmap=cmap, fmt=".2f", linewidths=.5, cbar_kws={'label': 'LOG_ProbofAntiForM'},annot_kws={"fontsize": 9})
+
+# 调整x轴字体和旋转角度
+plt.xticks(rotation=45, ha='right', fontsize=10)  # x轴标签字体小一些
+plt.yticks(fontsize=10)  # y轴标签字体小一些
+
+# 设置标题和标签
+plt.title('Conta_proportion', fontsize=20, weight='bold', ha='center',pad=20)
+plt.xlabel('adulterant', fontsize=15)  # x轴标题大一些
+plt.ylabel('monitoring_province', fontsize=15)  # y轴标题大一些
+
+# 设置图例标题字体大小
+colorbar = heatmap.collections[0].colorbar
+colorbar.ax.tick_params(labelsize=12)  # 调整图例标签字体大小
+colorbar.set_label('LOG_ProbofAntiForM', fontsize=15)  # 图例标题字体大一些
+# 调整布局
+plt.tight_layout(pad=2)
+plt.show()
--- vis.py
+++ vis.py
@@ -76,62 +76,62 @@ from bokeh.io import output_notebook
 # # Show the plot
 # fig.show()
 # show
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-# Load the Excel file
-file_path = '/Users/wangxiaoran/Desktop/副本20240625 画图需求.xlsx'
-data2 = pd.read_excel(file_path, sheet_name=1)
-
-# Renaming columns for easier handling
-data2.columns = ['MonitoringProvince', 'HIforMfromP', 'ProductionProvince']
-
-# Creating the pivot table to prepare the data for plotting
-pivot_data2 = data2.pivot_table(index='MonitoringProvince', columns='ProductionProvince', values='HIforMfromP', fill_value=0)
-
-# Calculate the total Hazard Index for each Monitoring Province for sorting
-pivot_data2['TotalHI'] = pivot_data2.sum(axis=1)
-
-# Sort the pivot table by the Total Hazard Index
-pivot_data2 = pivot_data2.sort_values('TotalHI', ascending=False)
-
-# Drop the TotalHI column after sorting
-pivot_data2 = pivot_data2.drop(columns=['TotalHI'])
-
-# Reset the index to use in Seaborn
-pivot_data2 = pivot_data2.reset_index()
-
-# Melt the dataframe for use in Seaborn
-melted_data = pivot_data2.melt(id_vars='MonitoringProvince', var_name='ProductionProvince', value_name='HIforMfromP')
-
-# Plotting the stacked bar plot
-plt.figure(figsize=(16, 12))
-sns.set_palette("Paired")
-
-# Create a horizontal barplot
-barplot = sns.barplot(data=melted_data, y='MonitoringProvince', x='HIforMfromP', hue='ProductionProvince', dodge=False)
-
-# Customizing the plot
-plt.title('The probability of contaminated aquatic products associated with antibiotics in each consumption region, along with the respective contribution proportions from various production regions.',fontsize=10)
-plt.xlabel('Hazard Index')
-plt.ylabel('Consumption region')
-
-# Remove the barplot frame
-barplot.spines['top'].set_visible(False)
-barplot.spines['right'].set_visible(False)
-barplot.spines['left'].set_visible(False)
-barplot.spines['bottom'].set_visible(False)
-
-# Remove the legend frame
-legend = plt.legend(title='Production Province', bbox_to_anchor=(1.05, 1), loc='upper left')
-legend.get_frame().set_linewidth(0.0)
-
-# Show the plot
-plt.tight_layout()
-plt.show()
-
-
+# import pandas as pd
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+#
+# # Load the Excel file
+# file_path = '/Users/wangxiaoran/Desktop/副本20240625 画图需求.xlsx'
+# data2 = pd.read_excel(file_path, sheet_name=1)
+#
+# # Renaming columns for easier handling
+# data2.columns = ['MonitoringProvince', 'HIforMfromP', 'ProductionProvince']
+#
+# # Creating the pivot table to prepare the data for plotting
+# pivot_data2 = data2.pivot_table(index='MonitoringProvince', columns='ProductionProvince', values='HIforMfromP', fill_value=0)
+#
+# # Calculate the total Hazard Index for each Monitoring Province for sorting
+# pivot_data2['TotalHI'] = pivot_data2.sum(axis=1)
+#
+# # Sort the pivot table by the Total Hazard Index
+# pivot_data2 = pivot_data2.sort_values('TotalHI', ascending=False)
+#
+# # Drop the TotalHI column after sorting
+# pivot_data2 = pivot_data2.drop(columns=['TotalHI'])
+#
+# # Reset the index to use in Seaborn
+# pivot_data2 = pivot_data2.reset_index()
+#
+# # Melt the dataframe for use in Seaborn
+# melted_data = pivot_data2.melt(id_vars='MonitoringProvince', var_name='ProductionProvince', value_name='HIforMfromP')
+#
+# # Plotting the stacked bar plot
+# plt.figure(figsize=(16, 12))
+# sns.set_palette("Paired")
+#
+# # Create a horizontal barplot
+# barplot = sns.barplot(data=melted_data, y='MonitoringProvince', x='HIforMfromP', hue='ProductionProvince', dodge=False)
+#
+# # Customizing the plot
+# plt.title('The probability of contaminated aquatic products associated with antibiotics in each consumption region, along with the respective contribution proportions from various production regions.',fontsize=10)
+# plt.xlabel('Hazard Index')
+# plt.ylabel('Consumption region')
+#
+# # Remove the barplot frame
+# barplot.spines['top'].set_visible(False)
+# barplot.spines['right'].set_visible(False)
+# barplot.spines['left'].set_visible(False)
+# barplot.spines['bottom'].set_visible(False)
+#
+# # Remove the legend frame
+# legend = plt.legend(title='Production Province', bbox_to_anchor=(1.05, 1), loc='upper left')
+# legend.get_frame().set_linewidth(0.0)
+#
+# # Show the plot
+# plt.tight_layout()
+# plt.show()
+#
+#
 
 
 import pandas as pd
@@ -148,7 +148,7 @@ data_avg_concentration = data3[['monitoring_province_y', 'adulterant_english_x',
 data_avg_concentration.columns = ['MonitoringProvince', 'Adulterant', 'AverageConcentration']
 
 # 取以2为底的log
-data_avg_concentration['LogAverageConcentration'] = np.log(data_avg_concentration['AverageConcentration'] + 1) / np.log(1.1)
+data_avg_concentration['LogAverageConcentration'] = np.log(data_avg_concentration['AverageConcentration'] + 1) / np.log(2)
 
 # 创建数据透视表
 pivot_data_avg_concentration_log = data_avg_concentration.pivot_table(index='MonitoringProvince', columns='Adulterant', values='LogAverageConcentration', fill_value=0)
@@ -162,7 +162,7 @@ cmap = "coolwarm"
 boundaries = np.linspace(0, 1, 100)
 norm = mcolors.BoundaryNorm(boundaries, ncolors=256)
 #作图
-heatmap = sns.heatmap(pivot_data_avg_concentration_log, annot=True, cmap=cmap, fmt=".2f", linewidths=.5, cbar_kws={'label': 'Log0.5(Average Concentration (ug/kg))'},annot_kws={"fontsize": 9})
+heatmap = sns.heatmap(pivot_data_avg_concentration_log, annot=True, cmap=cmap, fmt=".2f", linewidths=.5, cbar_kws={'label': 'Log2(Average Concentration (ug/kg))'},annot_kws={"fontsize": 9})
 
 # 调整x轴字体和旋转角度
 plt.xticks(rotation=45, ha='right', fontsize=10)  # x轴标签字体小一些
@@ -176,7 +176,7 @@ plt.ylabel('Consumption region', fontsize=15)  # y轴标题大一些
 # 设置图例标题字体大小
 colorbar = heatmap.collections[0].colorbar
 colorbar.ax.tick_params(labelsize=12)  # 调整图例标签字体大小
-colorbar.set_label('Log1.2(Average Concentration (ug/kg))', fontsize=15)  # 图例标题字体大一些
+colorbar.set_label('Log2(Average Concentration (ug/kg))', fontsize=15)  # 图例标题字体大一些
 # 调整布局
 plt.tight_layout(pad=2)
 plt.show()
